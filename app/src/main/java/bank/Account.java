6b7b4477e41623a322d243d7e9efee9ac79d2cba
Ruzeka Uddin <ruzekauddin@hotmail.co.uk>
2023-02-22 23:07:24 +0000

    Created a getBalance method that retrieves the current balance of the account and passes the test

---
+++ app/src/main/java/bank/Account.java
@@ -0,0 +1,14 @@
+package bank;
+
+public class Account {
+  private double balance;
+
+  public Account() {
+      this.balance = 0.00;
+  }
+
+  public double getBalance() {
+      return this.balance;
+  }
+}
+
