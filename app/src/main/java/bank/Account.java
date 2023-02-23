2fd10cafa0de676e4835983608d6ae7d70311033
Ruzeka Uddin <ruzekauddin@hotmail.co.uk>
2023-02-23 01:08:03 +0000

    Withdraw method is now created, and the test is passing.

--- app/src/main/java/bank/Account.java
+++ app/src/main/java/bank/Account.java
@@ -14,6 +14,9 @@ public class Account {
   public void deposit(double amount) {
     this.balance += amount;
   }
-
+  
+  public void withdraw(double amount) {
+    this.balance -= amount;
+  }
 }
 
