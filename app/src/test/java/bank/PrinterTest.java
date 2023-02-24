c057fa7c730a89e0c1febaf3ea3378fbb0bd1a6a
Ruzeka Uddin <ruzekauddin@hotmail.co.uk>
2023-02-24 20:44:12 +0000

    Setup PrinterTest for print string with failing test

--- app/src/test/java/bank/PrinterTest.java
+++ app/src/test/java/bank/PrinterTest.java
@@ -1,5 +1,16 @@
 package bank;
 
+import org.junit.Test;
+
 public class PrinterTest {
+
+    @Test
+      public void testPrintString() {
+          Printer printer = new Printer();
+          String expectedOutput = "Hello, world!";
+          assertEquals(expectedOutput, printer.print(expectedOutput));
+      }
   
-}
+      
+  
+  }
