fd484c4449ae807b3b77fc30c72d67a46267de43
Ruzeka Uddin <ruzekauddin@hotmail.co.uk>
2023-02-25 00:47:04 +0000

    Created Private methods to record deposit and withdraw transactions and updated both methods.

--- app/src/main/java/bank/Account.java
+++ app/src/main/java/bank/Account.java
@@ -1,5 +1,6 @@
 package bank;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Account {
@@ -9,6 +10,8 @@ public class Account {
   
   public Account() {
       this.balance = 0.00;
+      this.printer = new Printer();
+      this.statement = new ArrayList<>();
   }
 
   public double getBalance() {
@@ -17,6 +20,7 @@ public class Account {
  
   public void deposit(double amount) {
     this.balance += amount;
+    recordDeposit(amount, this.balance);
   }
   
   public void withdraw(double amount) {
@@ -24,12 +28,23 @@ public class Account {
       throw new IllegalArgumentException("Insufficient funds");
     }
     this.balance -= amount;
+    recordWithdrawal(amount, this.balance);
   }  
 
   public String printStatement() {
     printer.print("date || credit || debit || balance");
     printer.print(this.statement);
     return "Statement printed successfully";
+}
+
+  private void recordDeposit(double credit, double balance) {
+  Transaction amount = new Transaction(credit, 0.00, balance);
+  this.statement.add(amount.displayTemplate());
+}
+
+  private void recordWithdrawal(double debit, double balance) {
+  Transaction amount = new Transaction(0.00, debit, balance);
+  this.statement.add(amount.displayTemplate());
 }
   
 }
