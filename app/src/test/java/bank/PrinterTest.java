82806f07188b3fa7239792c62d8827f48108078e
Ruzeka Uddin <ruzekauddin@hotmail.co.uk>
2023-02-25 01:12:35 +0000

    Refactored the PrinterTest and all tests are now passing

--- app/src/test/java/bank/PrinterTest.java
+++ app/src/test/java/bank/PrinterTest.java
@@ -1,34 +1,39 @@
 package bank;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.Assert.assertEquals;
 
 public class PrinterTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
 
-    @Test
-       public void testPrintString() {
-       Printer printer = new Printer();
-       String expectedOutput = "Hello, world!";
-       ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-       System.setOut(new PrintStream(outContent));
-       printer.print(expectedOutput);
-       assertEquals(expectedOutput, outContent.toString().trim());
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void restoreStreams() {
+        System.setOut(originalOut);
     }
 
     @Test
-      public void testPrintList() {
-      Printer printer = new Printer();
-      List<String> items = new ArrayList<>();
-      items.add("Item 1");
-      items.add("Item 2");
-      items.add("Item 3");
-      assertTrue(printer.print(items));
-    } 
+    public void testPrintList() {
+        Printer printer = new Printer();
+        List<String> items = new ArrayList<>();
+        items.add("Item 1");
+        items.add("Item 2");
+        items.add("Item 3");
+        printer.print(items);
+        String expectedOutput = "Item 1\nItem 2\nItem 3\n";
+        assertEquals(expectedOutput, outContent.toString());
+    }
 }
