96aef948168cbb081c390566f22b259c1b1fc9bf
Ruzeka Uddin <ruzekauddin@hotmail.co.uk>
2023-02-25 01:09:33 +0000

    Added to the Transaction constructor that creates an instance of the Account class with an initial balance, printer, and statement. I also created a test to display debit temp and add relevant imports.

--- app/src/main/java/bank/Transaction.java
+++ app/src/main/java/bank/Transaction.java
@@ -1,11 +1,25 @@
 package bank;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Transaction {
+  private final LocalDate date;
+  private final double credit;
+  private final double debit;
+  private final double balance;
 
   public Transaction(double credit, double debit, double balance) {
-    LocalDate.now();
+      this.date = LocalDate.now();
+      this.credit = credit;
+      this.debit = debit;
+      this.balance = balance;
 }
-  
+
+public String displayTemplate() {
+  DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+  String dateFormatted = this.date.format(formatter);
+  return String.format("%s || %.2f || %.2f || %.2f", dateFormatted, this.credit, this.debit, this.balance);
+}
+
 }
--- app/src/test/java/bank/TransactionTest.java
+++ app/src/test/java/bank/TransactionTest.java
@@ -1,5 +1,7 @@
 package bank;
 
+import static org.junit.Assert.assertEquals;
+
 import java.time.LocalDate;
 
 import org.junit.Test;
@@ -12,4 +14,13 @@ public class TransactionTest {
         String expectedTemplate = String.format("%s || 100.00 || || 100.00", date.toString());
         assertEquals(expectedTemplate, transaction.displayTemplate());
     }
+
+    @Test
+    public void testDisplayTemplateForDebit() {
+        Transaction transaction = new Transaction(0.0, 50.0, 100.0);
+        LocalDate date = LocalDate.now();
+        String expectedTemplate = String.format("%s || || 50.00 || 100.00", date.toString());
+        assertEquals(expectedTemplate, transaction.displayTemplate());
+    }
+
 }
