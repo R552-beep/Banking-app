08663371332e9cd052f35872bd117914309c6a24
Ruzeka Uddin <ruzekauddin@hotmail.co.uk>
2023-02-25 00:59:21 +0000

    Defined a constructor method for the Transaction class and created a test for Displaytemp for credit added the relevant imports.

--- app/src/main/java/bank/Transaction.java
+++ app/src/main/java/bank/Transaction.java
@@ -1,5 +1,11 @@
 package bank;
 
+import java.time.LocalDate;
+
 public class Transaction {
+
+  public Transaction(double credit, double debit, double balance) {
+    LocalDate.now();
+}
   
 }
--- app/src/test/java/bank/TransactionTest.java
+++ app/src/test/java/bank/TransactionTest.java
@@ -1,5 +1,15 @@
 package bank;
 
+import java.time.LocalDate;
+
+import org.junit.Test;
+
 public class TransactionTest {
-  
+  @Test
+    public void testDisplayTemplateForCredit() {
+        Transaction transaction = new Transaction(100.0, 0.0, 100.0);
+        LocalDate date = LocalDate.now();
+        String expectedTemplate = String.format("%s || 100.00 || || 100.00", date.toString());
+        assertEquals(expectedTemplate, transaction.displayTemplate());
+    }
 }
