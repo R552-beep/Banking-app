9df882cbaf13420047e4b2ce539a889cb59d0da5
Ruzeka Uddin <ruzekauddin@hotmail.co.uk>
2023-02-26 23:19:18 +0000

    Had to ammend the all tests in transactionTest

--- app/src/test/java/bank/TransactionTest.java
+++ app/src/test/java/bank/TransactionTest.java
@@ -3,24 +3,36 @@ package bank;
 import static org.junit.Assert.assertEquals;
 
 import java.time.LocalDate;
-
+import java.time.format.DateTimeFormatter;
 import org.junit.Test;
 
 public class TransactionTest {
-  @Test
+
+@Test
     public void testDisplayTemplateForCredit() {
         Transaction transaction = new Transaction(100.0, 0.0, 100.0);
-        LocalDate date = LocalDate.now();
-        String expectedTemplate = String.format("%s || 100.00 || || 100.00", date.toString());
-        assertEquals(expectedTemplate, transaction.displayTemplate());
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate now = LocalDate.now();
+        String expected = String.format("%s || 100.00 || 0.00 || 100.00", now.format(formatter));
+        assertEquals(expected, transaction.displayTemplate());
     }
 
     @Test
     public void testDisplayTemplateForDebit() {
         Transaction transaction = new Transaction(0.0, 50.0, 100.0);
-        LocalDate date = LocalDate.now();
-        String expectedTemplate = String.format("%s || || 50.00 || 100.00", date.toString());
-        assertEquals(expectedTemplate, transaction.displayTemplate());
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate now = LocalDate.now();
+        String expected = String.format("%s || 0.00 || 50.00 || 100.00", now.format(formatter));
+        assertEquals(expected, transaction.displayTemplate());
+    }
+
+    @Test
+    public void testDisplayTemplateForCreditAndDebit() {
+        Transaction transaction = new Transaction(75.0, 50.0, 100.0);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate now = LocalDate.now();
+        String expected = String.format("%s || 75.00 || 50.00 || 100.00", now.format(formatter));
+        assertEquals(expected, transaction.displayTemplate());
     }
 
 }
